2c448da7ca9db93c64fbde7cc0bfcf44cb91cc64
Ashish Sharma <asharma4@isimtl.local>
2020-08-22 18:25:41 -0400

    third

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+    <title>First php page</title>
+</head>
+
+<body>
+
 <?php
-echo 'Helo World';
+$x = 10;
+$x = 'sss'; //variable decleration
+echo 'Helo World<br>';
+echo 'ashish sharma';
+?>
+</body>
+</html>
